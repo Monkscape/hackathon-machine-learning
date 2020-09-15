8378b84b4b2c6762c5d2499d362a1636ba91408b
Monkscape <noahmillermonkscape@gmail.com>
2020-09-15 18:17:55 -0500

    Update leads_server to support multiple prospects

--- leads_server.py
+++ leads_server.py
@@ -7,10 +7,10 @@ app = Flask('leads')
 with open('leads-model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-def predict_single(prospect, dv, model):
-    X = dv.transform([prospect])
+def predict_prospects(prospects, dv, model):
+    X = dv.transform(prospects)
     y_pred = model.predict_proba(X)[:,1]
-    return y_pred[0]
+    return y_pred
 
 @app.route('/ping', methods=['GET'])
 def ping():
@@ -18,17 +18,18 @@ def ping():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    customer = request.get_json()
+    prospects = request.get_json()
  
-    prediction = predict_single(customer, dv, model)
-    conversion = prediction >= 0.5
+    predictions = predict_prospects(prospects, dv, model)
     
-    result = {
-        'lead_probability': float(prediction),
-        'conversion': bool(conversion),
-    }
- 
-    return jsonify(result)
+    prediction_results = []
+    for prediction in predictions:
+        prediction_results.append({
+            'lead_probability': float(prediction),
+            'conversion': bool(prediction >= 0.5),
+        })
+
+    return jsonify(prediction_results)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
